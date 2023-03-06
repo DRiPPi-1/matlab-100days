0d7f7b87b90ce107c4f8f171ec636d0f9367355c
Hlumelo Siyo <hlumelosiyo2@gmail.com>
2023-03-06 21:26:16 +0200

    Modified day2

--- day2_single_digit.m
+++ day2_single_digit.m
@@ -2,12 +2,14 @@
 % GitHub username: DRiPPi-1
 % Day 2/100: Get a Single Digit
 
-clear
-n=input("Enter positive integer: ");
-if n > 0
-    fprintf('%d \n', n);
-else
-    fprintf('%s \n', 'Invalid');
-    n=input("Enter positive integer: ");
+function num = day2_single_digit()
+num = 0;
+    while num == 0 % the loop will continue until input is 1 to 9 single digit
+        n=input("Enter positive integer: ");
+        if (n > 0) && (n <= 9) && (mod(n, 1) == 0) % check if the input is between 1 to 9 and single digit
+            num = n;
+        else
+            fprintf('%s \n', 'Invalid'); % print Invalid if not single digit
+        end
+    end
 end
-% try do-while
